312c70f3268649d84fd1d58a3d242f9f48cc78eb
juancmdev <juancmorenoj@gmail.com>
2025-03-11 20:18:19 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db";
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
         <div>
           <h2>Menú</h2>
+          {menuItems.map((item) => (
+            <MenuItem key={item.id} item={item} />
+          ))}
         </div>
         <div>
           <h2>Consumo</h2>
