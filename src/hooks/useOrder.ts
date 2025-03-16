d86fc6605fc45a4a3e3123a322b1f83fb80d9ab2
juancmdev <juancmorenoj@gmail.com>
2025-03-16 14:14:50 -0500

    Update useOrder.ts

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -8,6 +8,12 @@ export default function useOrder() {
     const itemExists = order.find((orderItem) => orderItem.id === item.id);
 
     if (itemExists) {
+      const updatedOrder = order.map((orderItem) =>
+        orderItem.id === item.id
+          ? { ...orderItem, quantity: orderItem.quantity + 1 }
+          : orderItem
+      );
+      setOrder(updatedOrder);
     } else {
       const newItem = { ...item, quantity: 1 };
       setOrder([...order, newItem]);
