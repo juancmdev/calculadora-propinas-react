b5d4c308cf48d7207dbfd18f2bcb2778c3818c1f
juancmdev <juancmorenoj@gmail.com>
2025-03-21 19:34:29 -0500

    update

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -8,7 +8,19 @@ export default function OrderContents({ order }: OrderContentsProps) {
   return (
     <div>
       <h2 className="font-black text-4xl">Consumo</h2>
-      <div className="space-y-3 mt-5"></div>
+      <div className="space-y-3 mt-5">
+        {order.length === 0 ? (
+          <p className="text-center">La orden está vacía</p>
+        ) : (
+          order.map((item) => (
+            <div key={item.id}>
+              <p>
+                {item.name} x {item.quantity}
+              </p>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -20,8 +20,6 @@ export default function useOrder() {
     }
   };
 
-  console.log(order);
-
   return {
     order,
     addItem,
