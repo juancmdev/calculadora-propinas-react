d0387b8555bad050b8cae2437100a16463d28bd5
juancmdev <juancmorenoj@gmail.com>
2025-03-24 14:12:43 -0500

    Update TipPercentageForm.tsx

--- src/components/TipPercentageForm.tsx
+++ src/components/TipPercentageForm.tsx
@@ -22,7 +22,7 @@ export default function TipPercentageForm() {
       <h3 className="font-black text-2xl">Propina</h3>
       <form>
         {tipOptions.map((tip) => (
-          <div>
+          <div key={tip.id} className="flex gap-2">
             <label htmlFor={tip.id}>{tip.label}</label>
             <input id={tip.id} type="radio" name="tip" value={tip.value} />
           </div>
