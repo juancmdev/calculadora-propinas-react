d30661cd26ba1b1d23df89821e936900c27f3037
juancmdev <juancmorenoj@gmail.com>
2025-03-24 13:42:23 -0500

    Update OrderTotals.tsx

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -4,7 +4,15 @@ export default function OrderTotals() {
       <div className="space-y-3">
         <h2 className="font-black text-2xl">Totales y propina:</h2>
         <p>
-          Subtotal a pagar:
+          Subtotal a pagar: {""}
+          <span className="font-bold">$0</span>
+        </p>
+        <p>
+          Propina: {""}
+          <span className="font-bold">$0</span>
+        </p>
+        <p>
+          Total a pagar: {""}
           <span className="font-bold">$0</span>
         </p>
       </div>
