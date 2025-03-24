622035e917c7e82e35bdd2794307b8458ea16729
juancmdev <juancmorenoj@gmail.com>
2025-03-24 13:20:21 -0500

    Update OrderContents.tsx

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -14,14 +14,22 @@ export default function OrderContents({ order }: OrderContentsProps) {
           <p className="text-center">La orden está vacía</p>
         ) : (
           order.map((item) => (
-            <div key={item.id}>
-              <p className="text-lg">
-                {item.name} - {formatCurrency(item.price)}
-              </p>
-              <p className="font-black">
-                Cantidad: {item.quantity} -{" "}
-                {formatCurrency(item.price * item.quantity)}
-              </p>
+            <div
+              key={item.id}
+              className="flex justify-between border-t border-gray-200 py-5 last-of-type:border-b items-ce"
+            >
+              <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
+                <p className="font-black">
+                  Cantidad: {item.quantity} -{" "}
+                  {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
+              <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black">
+                X
+              </button>
             </div>
           ))
         )}
