3efbe024bf0a90e14b6a2c8e2bc738c6b0fc72a2
juancmdev <juancmorenoj@gmail.com>
2025-04-05 19:24:34 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,16 @@ function App() {
           </div>
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded space-y-10">
-          <OrderContents order={order} removeItem={removeItem} />
+          {order.length >
+            0(
+              <>
+                <OrderContents order={order} removeItem={removeItem} />
 
-          <TipPercentageForm setTip={setTip} tip={tip} />
+                <TipPercentageForm setTip={setTip} tip={tip} />
 
-          <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
+                <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
+              </>
+            )}
         </div>
       </main>
     </>
