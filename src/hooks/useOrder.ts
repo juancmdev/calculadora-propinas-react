fa335372f701a6789f6f381a1594051e4717f859
juancmdev <juancmorenoj@gmail.com>
2025-04-06 09:44:44 -0500

    Update useOrder.ts

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import type { MenuItem, OrderItem } from "../types";
-import { menuItems } from "../data/db";
+// import { menuItems } from "../data/db";
 
 export default function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([]);
